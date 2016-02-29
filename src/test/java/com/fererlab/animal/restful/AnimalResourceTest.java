e96eea10337c50bc44407f5ee1407db1542f921c
canmogol <canmogol@gmail.com>
2016-02-29 16:04:36 +0200

    testing codecov.io integration

--- src/test/java/com/fererlab/animal/restful/AnimalResourceTest.java
+++ src/test/java/com/fererlab/animal/restful/AnimalResourceTest.java
@@ -1,15 +1,28 @@
 package com.fererlab.animal.restful;
 
+import com.fererlab.animal.dto.GenericList;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class AnimalResourceTest {
 
+    private AnimalResource animalResource;
+
+    @Before
+    public void prepare() {
+        animalResource = new AnimalResource();
+    }
+
     @Test
     public void testHi() throws Exception {
-        AnimalResource animalResource = new AnimalResource();
         Assert.assertEquals(animalResource.hi(), "Hello");
     }
 
+    @Test
+    public void testList() throws Exception {
+        GenericList list = animalResource.list();
+        Assert.assertEquals(list.getList().size(), 2);
+    }
 }
